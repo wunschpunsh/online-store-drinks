6b66b86f7c36e00cd16c1aa27b0412e1dd1fd119
Artem Salnikov <88932070+wunschpunsh@users.noreply.github.com>
2022-10-11 00:04:51 +0300

    сделана полная верстка мобильной версии

---
+++ source/js/menu-toggle.js
@@ -0,0 +1,11 @@
+const menuButton = document.querySelector(".main-header__button");
+const naviBar = document.querySelector(".main-header__nav");
+
+const toggleMenu = () => {
+  menuButton.addEventListener("click", () => {
+    naviBar.classList.toggle("main-nav--opened");
+    console.log("click");
+  });
+};
+
+export { toggleMenu };
